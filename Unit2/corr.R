84e5b9a90241032afcae4b959d1c92780ddc0f7a
Alessandra Gherardelli <alessandra.gherardelli@gmail.com>
2023-04-24 09:53:11 +0200

    Buggy corr.R

---
+++ Unit2/corr.R
@@ -0,0 +1,24 @@
+# Write a function that takes a directory of data files and a threshold for complete cases and calculates the correlation between sulfate and nitrate for monitor locations where the number of completely observed cases (on all variables) is greater than the threshold. The function should return a vector of correlations for the monitors that meet the threshold requirement. If no monitors meet the threshold requirement, then the function should return a numeric vector of length 0. A prototype of this function follows
+
+
+corr <- function(directory,threshold = 0) {
+  # create vector
+  output = numeric()
+  path <- paste0(getwd(), "/",directory)
+  # read file
+  for (file in list.files(path, full.names = TRUE)){
+    tmp <- read.csv(file)
+    tmp <-na.omit(tmp)
+    nobs <- nrow(tmp)
+    if (nobs > threshold){
+      correlations <- corr(tmp$sulfate, tmp$nitrate)
+      output <- c(output, correlations)
+    } else{
+      NULL
+    }
+
+  }
+  }
+
+  
+  
